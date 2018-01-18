7f19031e139fda13733fb1168cb3e5e46299e395
yilmazoguzhan <oguzhaanyilmaz@gmail.com>
2018-01-18 21:26:09 +0300

    Sütun üzerinde filtreleme yapmak.(Like Komutu)

---
+++ Like arama yapmak.sql
@@ -0,0 +1,11 @@
+SELECT * FROM WehicleBrands
+WHERE Brand LIKE 'FORD'
+
+SELECT * FROM WehicleBrands
+WHERE Brand LIKE '%RD'
+
+SELECT * FROM WehicleBrands
+WHERE Brand LIKE 'FOR%'
+
+SELECT * FROM WehicleBrands
+WHERE Brand LIKE '%OR%'
