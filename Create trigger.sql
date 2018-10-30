78b2a569086f1795e7fbb7f111a22843f18e0bd0
Oğuzhan Yılmaz <oguzhaanyilmaz@gmail.com>
2018-10-30 14:11:02 +0300

    Create trigger.

---
+++ Create trigger.sql
@@ -0,0 +1,36 @@
+-- ================================================
+-- Template generated from Template Explorer using:
+-- Create Trigger (New Menu).SQL
+--
+-- Use the Specify Values for Template Parameters 
+-- command (Ctrl-Shift-M) to fill in the parameter 
+-- values below.
+--
+-- See additional Create Trigger templates for more
+-- examples of different Trigger statements.
+--
+-- This block of comments will not be included in
+-- the definition of the function.
+-- ================================================
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+-- =============================================
+-- Author:		<Author,,Name>
+-- Create date: <Create Date,,>
+-- Description:	<Description,,>
+-- =============================================
+CREATE TRIGGER <Schema_Name, sysname, Schema_Name>.<Trigger_Name, sysname, Trigger_Name> 
+   ON  <Schema_Name, sysname, Schema_Name>.<Table_Name, sysname, Table_Name> 
+   AFTER <Data_Modification_Statements, , INSERT,DELETE,UPDATE>
+AS 
+BEGIN
+	-- SET NOCOUNT ON added to prevent extra result sets from
+	-- interfering with SELECT statements.
+	SET NOCOUNT ON;
+
+    -- Insert statements for trigger here
+
+END
+GO
